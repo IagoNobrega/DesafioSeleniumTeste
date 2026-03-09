6688bca9cd2c13c9b28c82ef9a81557a4f5d3c97
IagoNobrega <iagon.araujo14@gmail.com>
2026-03-09 19:48:26 -0300

    Initial commit

---
+++ src/test/java/steps/QazandoSteps.java
@@ -0,0 +1,11 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+
+public class CertificadoQAsteps {
+
+    @Dado("~que estou no site da qazando")
+    public void acessar_site_qazando(){
+
+    }
+}
